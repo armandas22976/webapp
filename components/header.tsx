ffd38f84848a17b7f19f5a011dee4f59e64d4a75
as22976 <as22976@essex.ac.uk>
2025-03-26 18:34:24 +0000

    small

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="container mx-auto px-4 sm:px-6 flex items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
           <Lock className="h-6 w-6 text-primary" />
-          <span className="text-xl font-bold">Secure File Share</span>
+          <span className="text-xl font-bold">Anon File Share</span>
         </Link>
         
         <nav className="flex items-center space-x-4">
